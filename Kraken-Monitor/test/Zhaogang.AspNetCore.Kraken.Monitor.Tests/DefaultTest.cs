8f82c34aff633cf214442bbd0c0d554fe0dc6b86
ding.ding <ding.ding@china.zhaogang.com>
2016-06-28 15:28:37 +0800

    init projects

---
+++ Kraken-Monitor/test/Zhaogang.AspNetCore.Kraken.Monitor.Tests/DefaultTest.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Zhaogang.AspNetCore.Kraken.Monitor.Tests
+{
+    [TestClass]
+    public class DefaultTest
+    {
+        public DefaultTest()
+        {
+        }
+
+        [TestMethod]
+        public void DefaultTester() { }
+    }
+}
